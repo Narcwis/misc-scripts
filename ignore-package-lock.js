f68fdb5d0f06791c92fbdae2fd36fbd1bbcab334
Narcis Cristea <narcis.cristea@gmail.com>
2019-05-14 13:54:38 +0100

    Create ignore-package-lock.js
    This will set the `display` property of the package-lock.json section on PRs to `none`

---
+++ ignore-package-lock.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Remove package-lock from bitbucket
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Daffodil
+// @match        https://bitbucket.org/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.4.1.slim.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let interval = setInterval(() => {
+
+         if ($('section[data-path="package-lock.json"]').css('display') !== 'none') {
+             console.log('element detected');
+             $('section[data-path="package-lock.json"]').css('display', 'none' );
+         } else {
+             clearInterval(interval);
+         }
+    }, 300);
+})();
