5daaafc836a9c32c8ca7654bf8621f7280f65460
Narcis Cristea <narcis.cristea@gmail.com>
2019-05-14 15:44:29 +0100

    now shows button to unhide package-lock changes

--- ignore-package-lock.js
+++ ignore-package-lock.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove package-lock from bitbucket
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  try to take over the world!
 // @author       Daffodil
 // @match        https://bitbucket.org/*
@@ -11,11 +11,20 @@
 
 (function() {
     'use strict';
+    const showPackageLock = () => {
+        console.log(window.packageLock);
+        $('section[data-path="package-lock.json"]').css('display', 'unset');
+        $('button[class="execute click aui-button aui-button-light sbs package-lock"]').css('display', 'none' );
+    };
+    window.showPackageLock = showPackageLock;
+    let element;
     let interval = setInterval(() => {
 
          if ($('section[data-path="package-lock.json"]').css('display') !== 'none') {
-             console.log('element detected');
+             window.packageLock = $('section[data-path="package-lock.json"]');
+             console.log('element detected', $('section[data-path="package-lock.json"]'));
              $('section[data-path="package-lock.json"]').css('display', 'none' );
+             $('section[data-path="package-lock.json"]').after('<button onClick="window.showPackageLock()" class="execute click aui-button aui-button-light sbs package-lock">Show package-lock.json</button>');
          } else {
              clearInterval(interval);
          }
