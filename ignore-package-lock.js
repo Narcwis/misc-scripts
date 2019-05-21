a443a78e07d1ebad11c3e850f4871e5dc8c8b953
Narcis Cristea <narcis.cristea@gmail.com>
2019-05-21 09:06:02 +0100

    added logging

--- ignore-package-lock.js
+++ ignore-package-lock.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove package-lock from bitbucket
 // @namespace    http://tampermonkey.net/
-// @version      0.1.8
+// @version      0.1.9
 // @description  try to take over the world!
 // @author       Daffodil
 // @match        https://bitbucket.org/*
@@ -14,8 +14,11 @@
     let interval = setInterval(() => {
 
          if ($('article[aria-label="Diff of file package-lock.json"] > div > div[class*="rah-static"]').hasClass('rah-static--height-auto')) {
+             console.log('Should click on element');
              $('article[aria-label="Diff of file package-lock.json"] > div > div[data-qa="bk-file__header"]').click();
+             console.log('Should have clicked on element');
          } else {
+             console.log('Should have cleared the interval');
              clearInterval(interval);
          }
     }, 1000);
