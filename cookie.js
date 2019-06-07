10d08bf8ddef548223c24ed6c9a854633a5c6c87
Narcis Cristea <narcis.cristea@gmail.com>
2019-06-08 00:31:17 +0100

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Clicker Auto
 // @namespace    https://github.com/Narcwis/misc-scripts/blob/master/cookie.js
-// @version      0.0.6
+// @version      0.0.7
 // @description  try to take over the world!
 // @author       You
 // @match        https://orteil.dashnet.org/cookieclicker/
@@ -14,7 +14,7 @@
     setInterval(()=> {
     const toClick = $('div[class*="enabled"]');
     Object.keys(toClick).forEach((key) => {
-        if (toClick[key] === toClick[Object.keys(toClick)[Object.keys(toClick).length]]) {
+        if (toClick[key] === toClick[Object.keys(toClick)[Object.keys(toClick).length - 1]]) {
             toClick[key].click();
         }
     })
