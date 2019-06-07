88aeb5aca2e856e6e23db192ff928cd3110547f8
Narcis Cristea <narcis.cristea@gmail.com>
2019-06-07 14:07:37 +0100

    Create cookie.js

---
+++ cookie.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Clicker Auto
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://orteil.dashnet.org/cookieclicker/
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.4.1.slim.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(()=> {
+    const toClick = $('div[class*="enabled"]');
+    Object.keys(toClick).forEach((key) => {
+        if (toClick[key] !== toClick[Object.keys(toClick)[0]]) {
+            console.log('clicked');
+            console.log(toClick);
+            toClick[key].click();
+        }
+    })
+    }, 1000);
+})();
