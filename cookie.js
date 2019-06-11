b79dd2a303ec8361bf584e65417c115b96ec628d
Narcis Cristea <narcis.cristea@gmail.com>
2019-06-11 16:24:11 +0100

    added babushka check

--- cookie.js
+++ cookie.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Clicker Auto
 // @namespace    https://github.com/Narcwis/misc-scripts/blob/master/cookie.js
-// @version      0.0.10
+// @version      0.0.11
 // @description  try to take over the world!
 // @author       You
 // @match        https://orteil.dashnet.org/cookieclicker/
@@ -16,6 +16,15 @@
     }
     const clickStuff = (toClick) => {
         Object.keys(toClick).forEach((key) => {
+            if (toClick[key].id === 'product1'){
+                toClick[key].click();
+                sleep(300).then(() => {
+                    toClick[key].click();
+                    sleep(300).then(()=> {
+                        toClick[key].click();
+                    });
+                });
+            }
             if (toClick[key] === toClick[Object.keys(toClick)[Object.keys(toClick).length - 3]]) {
                 toClick[key].click();
                 sleep(300).then(() => {
@@ -30,7 +39,7 @@
     setInterval(()=> {
         const productsToClick = $('div[class*="product unlocked enabled"]');
         const upgradesToClick = $('div[class*="crate upgrade enabled"]');
-        clickStuff(upgradesToClick); 
+        clickStuff(upgradesToClick);
         clickStuff(productsToClick);
     }, 30000);
 })();
